fe4cf97eb9eca06c6502092cc26736cdf065e282
Cotaroy Cheung <twinflame265@gmail.com>
2025-11-18 15:16:13 -0500

    added getters and setters to move entity

--- src/main/java/entity/Move.java
+++ src/main/java/entity/Move.java
@@ -16,4 +16,42 @@ public class Move {
         this.type_id = type_id;
         this.damage = damage;
     }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPower() {
+        return power;
+    }
+    public void setPower(int power) {
+        this.power = power;
+    }
+    public int getAccuracy() {
+        return accuracy;
+    }
+    public void setAccuracy(int accuracy) {
+        this.accuracy = accuracy;
+    }
+    public String getEffect() {
+        return effect;
+    }
+    public void setEffect(String effect) {
+        this.effect = effect;
+    }
+    public int getType_id() {
+        return type_id;
+    }
+    public void setType_id(int type_id) {
+        this.type_id = type_id;
+    }
+    public int getDamage() {
+        return damage;
+    }
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
 }
