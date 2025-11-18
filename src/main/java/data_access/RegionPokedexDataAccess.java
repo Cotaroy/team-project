f994c139af6dbc2a661ae2e9509cfcaaf183c1f4
Nubcoder0118 <brad.hu.2006@gmail.com>
2025-11-18 12:35:43 -0500

    added regionpokedex data access into the data access fodler

--- src/main/java/data_access/RegionPokedexDataAccess.java
+++ src/main/java/data_access/RegionPokedexDataAccess.java
@@ -1,17 +1,15 @@
-package use_case.seeRegionPokedex;
+package data_access;
 
-import entity.Pokemon;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-import entity.Pokemon;
+import use_case.seeRegionPokedex.RegionPokedexDataAccessInterface;
 
 public class RegionPokedexDataAccess implements RegionPokedexDataAccessInterface {
     private final OkHttpClient client = new OkHttpClient();
