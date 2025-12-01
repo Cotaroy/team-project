c10bfe158193cb35cf2ce6858a20fd44d90d90b8
Nubcoder0118 <brad.hu.2006@gmail.com>
2025-11-30 21:55:36 -0500

    added a failure test case for seeRegionPokedex and updated the main page ui for connection to the page

--- src/main/java/interfaceadapter/main_menu/MainMenuPresenter.java
+++ src/main/java/interfaceadapter/main_menu/MainMenuPresenter.java
@@ -1,34 +1,33 @@
 package interfaceadapter.main_menu;
 
 import interfaceadapter.ViewManagerModel;
+import interfaceadapter.pokemonlookup.PokemonLookupViewModel;
 import interfaceadapter.teambuilder.TeamBuilderViewModel;
 import usecase.main_menu.MainMenuOutputBoundary;
 import usecase.main_menu.MainMenuOutputData;
+import view.PokemonLookupView;
 
 public class MainMenuPresenter implements MainMenuOutputBoundary {
     private final MainViewModel menuViewModel;
 
     private final ViewManagerModel viewManagerModel;
-//    private final Runnable switchToPokedex;
+    private final PokemonLookupViewModel pokemonLookupViewModel;
     private final TeamBuilderViewModel teamViewModel;
 
     public MainMenuPresenter(MainViewModel vm, ViewManagerModel viewManagerModel,
-//                             Runnable switchToPokedex,
+                             PokemonLookupViewModel pokemonLookupViewModel,
                              TeamBuilderViewModel teamViewModel) {
         this.menuViewModel = vm;
         this.viewManagerModel = viewManagerModel;
-//        this.switchToPokedex = switchToPokedex;
+        this.pokemonLookupViewModel = pokemonLookupViewModel;
         this.teamViewModel = teamViewModel;
     }
 
-//    @Override
-//    public void preparePokedexView(MainMenuOutputData data) {
-//        switchToPokedex.run();
-//    }
 
     @Override
     public void preparePokedexView(MainMenuOutputData data) {
-
+        viewManagerModel.setState(pokemonLookupViewModel.getViewName());
+        viewManagerModel.firePropertyChange();
     }
 
     @Override
