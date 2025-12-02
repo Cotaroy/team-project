c14e7719e7abb7cb660744b920f3176f578b7177
NathanFermin <nathan.fermin@mail.utoronto.ca>
2025-12-01 23:14:48 -0500

    fixed the title

--- src/main/java/view/HomePageView.java
+++ src/main/java/view/HomePageView.java
@@ -13,20 +13,26 @@ public class HomePageView extends JPanel{
     public HomePageView(MainViewModel viewModel) {
 
         JPanel home = new JPanel();
+        JPanel titlePage = new JPanel();
         home.setLayout(new GridLayout(2, 1, 20,20));
+        this.setLayout(new BorderLayout());
+        JPanel centerWrapper = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        centerWrapper.add(home);
 
         final JButton pokedexButton = new JButton("Pokedex");
         final JButton teamsButton = new JButton("Teams");
         final ImageIcon icon = new ImageIcon("src/main/resources/metagross.png");
         final JLabel title = new JLabel(icon);
+        title.setPreferredSize(new Dimension(1000, 300));
 
         viewname = viewModel.getViewName();
 
-        home.add(title);
+        titlePage.add(title);
         home.add(pokedexButton);
         home.add(teamsButton);
 
-        this.add(home, BorderLayout.CENTER);
+        this.add(centerWrapper, BorderLayout.CENTER);
+        this.add(titlePage, BorderLayout.NORTH);
 
         pokedexButton.addActionListener(
                 e -> controller.switchToPokedex());
